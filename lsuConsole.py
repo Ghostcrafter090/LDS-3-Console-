fe380f318dd9f25a46df23d0dc5f154265be5050
Joshua Jones <jjones@intertalksystems.com>
2022-07-29 08:50:00 -0300

    Fixed issues.

--- lsuConsole.py
+++ lsuConsole.py
@@ -5,6 +5,7 @@ import pytools
 
 class log:
     def write(data):
+        print(data.replace("\n", ""))
         dateArray = pytools.clock.getDateTime()
         string = str(dateArray[0]) + "-" + str(dateArray[1]) + "-" + str(dateArray[2])
         try:
@@ -15,22 +16,22 @@ class log:
 class arduino:
     def connect():
         comNumber = -1
-        while comNumber == -1:
-            comNumber = 4
-            if comNumber == -1:
+        while (comNumber < 100) and (arduino.conn == False):
+            comNumber = comNumber + 1
+            try:
+                arduino.conn = serial.Serial(port='COM' + str(comNumber), baudrate=115200, timeout=.1)
+            except:
                 log.write("Connection Faliure. Device not detected.")
-                time.sleep(10)
+                time.sleep(1)
                 log.write("checking again...")
-        arduino.conn = serial.Serial(port='COM' + str(comNumber), baudrate=115200, timeout=.1)
 
     conn = False
 
     def write(x):
-        global arduino
-        arduino.write(bytes(x, 'utf-8'))
+        arduino.conn.write(bytes(x, 'utf-8'))
 
-    def read(arduinon):
-        data = arduinon.readline()
+    def read():
+        data = arduino.conn.readline()
         return data
 
 def main():
@@ -43,10 +44,9 @@ def main():
             #     ard_write(string[i])
             #     time.sleep(0.0001)
             #     i = i + 1
-            out = arduino.read(arduino)
+            out = arduino.read()
             try:
                 if out[0] == 123:
-                    print(str(out.replace(b'}\r\n', b'}'))[2:][:-1])
                     dateArray = pytools.clock.getDateTime()
                     string = str(dateArray[0]) + "-" + str(dateArray[1]) + "-" + str(dateArray[2])
                     try:
